af8432a2cd78eba947f5a032850d1090eb027cc8
Ahmad Fikri <43110908+AhmadFikriAS@users.noreply.github.com>
2023-03-15 22:05:41 +0800

    update null safety

--- lib/maputils.dart
+++ lib/maputils.dart
@@ -3,11 +3,11 @@ import 'package:url_launcher/url_launcher.dart';
 class MapUtils {
   MapUtils._();
 
-  static Future<void> openMap(double latitude, double longtitude) async {
+  static Future<void> openMap(double? latitude, double? longtitude) async {
     String googleMapUrl =
         "https://www.google.com/maps/search/?api=1&query=$latitude,$longtitude";
 
-    if (await canLaunchUrl(Uri.parse(googleMapUrl))) {
+    if (!await canLaunchUrl(Uri.parse(googleMapUrl))) {
       await launchUrl(Uri.parse(googleMapUrl));
     } else {
       throw 'Could not open the map.';
